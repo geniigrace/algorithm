cededfca8fc97b61c793a77f298334d029cb4b7f
genii grace <jeyeom10@naver.com>
2022-09-23 01:11:03 +0900

    인프런 string 01

--- src/algorithm_java/Main01.java
+++ src/algorithm_java/Main01.java
@@ -3,24 +3,7 @@ package algorithm_java;
 import java.util.Scanner;
 
 public class Main01 {
-	public int solution (String str, char t) {
-		int answer=0;
-		
-		//대소문자 구분 없으므로 모두 문자로 변환한 다음 탐색 
-		// 대문자로 변환할 때는 toUpperCase
-		str=str.toLowerCase(); 
-		t=Character.toLowerCase(t); 
-		
-		//배열을 활용해 문자를 처음부터 끝까지 비교한 후, 동일할 때만 answer 값을올린다. 
-		for(int i=0; i<str.length(); i++) {
-			if(str.charAt(i)==t) {
-				answer++;
-			}
-		}
-		
-		
-		return answer;
-	}//답을 제출하는 형태일 뿐 
+
 	
 	public static void main(String[] agrs) {
 		
@@ -28,25 +11,37 @@ public class Main01 {
 		
 		/*  1. 문자열 입력받기
 		 *  2. 찾을 문자 입력받기
-		 *  3. 몇개인지 찾아서 출력하기 (대소문자 구분 안함) 
+		 *  3. 대소문자 구분 안함
+		 *  4. 찾는 문자 카운팅 하기 
 		 * */
-		Main01 T = new Main01();
+
 		
-		
-		// 스캐너 객체 생성 
 		Scanner sc = new Scanner(System.in);
 		
-		//문자열 받기
-		System.out.print("문자열을 입력하세요 : ");
+		// 문자열 입력받기 
 		String str = sc.next();
 		
-		//문자 입력 받기 
-		System.out.print("찾을 문자를 입력하세요 : ");
-		char c= sc.next().charAt(0);// 입력받은 문자열 중 0번째 문자 한개 읽어오기 
+		// 찾을 문자 입력받기 
+		char c = sc.next().charAt(0); // 입력받는 문자열 중 0번째 한글자 받아서 c 에 저장
 		
-		System.out.println("찾는 문자는 총 "+T.solution(str, c)+"개 입니다.");
+		//대소문자 구분 안함 -> 대문자 혹은 소문자로 모두 변환 
+		str=str.toUpperCase(); //대문자로 변환 
+		//str=str.toLowerCase(); //소문자로 변환 
+		c=Character.toUpperCase(c); //대문자로 변환 
+		//c=Character.toLowerCase(c); //소문자로 변환
 		
-		 
+		//System.out.println(str+" ,"+c); //대소문자 변환 확인
+		
+		// 찾는 문자 카운팅 하기 
+		int count=0;
+		for(int i=0; i<str.length();i++) { //문자열 길이만큼 탐색 
+			if(str.charAt(i)==c) { //문자열 배열에 i번째 글자가 입력받은 문자와 같다면 
+				count++; //카운트를 올림 
+				
+			}
+		}
+		System.out.println(count);
+		
 	}
 
 }
