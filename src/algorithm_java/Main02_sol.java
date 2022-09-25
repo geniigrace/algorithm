d9902004996dcbbea27fac3a008fab8961e94997
genii grace <jeyeom10@naver.com>
2022-09-26 01:33:15 +0900

    algorithm1-02 강의 풀이

---
+++ src/algorithm_java/Main02_sol.java
@@ -0,0 +1,30 @@
+package algorithm_java;
+
+import java.util.Scanner;
+
+class Main02_sol {
+	public String solution(String str) {
+		
+		String answer="";
+		
+		for(char x : str.toCharArray()) { // 문자를 배열로 바꿔주고 문자x로 배열 탐색  
+			if(Character.isLowerCase(x)) {//문자가 소문자인지 확인 
+				answer+=Character.toUpperCase(x); //대문자로 저장 
+			}
+			else { //문자가 대문자이면 
+				answer+=Character.toLowerCase(x); //소문자로 저장 
+			}
+		}
+		
+		return answer;
+	}
+	
+
+public static void main(String[] args) {
+	
+	Main02_sol T = new Main02_sol();
+	Scanner sc = new Scanner(System.in);
+	String str=sc.next();
+	System.out.print(T.solution(str));
+	}
+}
